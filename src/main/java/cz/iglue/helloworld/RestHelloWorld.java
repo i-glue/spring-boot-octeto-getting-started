e79b51a73192160a5459260730f72d02b93faa5f
tszappanosnt\tszappanos <tibor.szappanos@notix.cz>
2023-03-24 20:33:48 +0100

    inital commit

---
+++ src/main/java/cz/iglue/helloworld/RestHelloWorld.java
@@ -0,0 +1,13 @@
+package cz.iglue.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestHelloWorld {
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
